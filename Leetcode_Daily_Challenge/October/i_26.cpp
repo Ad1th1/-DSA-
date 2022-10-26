92853e43a7be93958a7f805e89617ef19ceb476a
Adithi <adithibhatindia@gmail.com>
2022-10-26 21:43:10 +0530

    Create i_26.cpp

---
+++ Leetcode_Daily_Challenge/October/i_26.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/continuous-subarray-sum/submissions/
+
+int prefSum = 0;
+	unordered_map<int, int> mp;
+	for(int i=0; i<nums.size(); i++)
+	{
+		prefSum += nums[i];
+		prefSum %= k;
+
+		if(prefSum == 0 && i) return true;
+
+		// cout << prefSum << " ";
+		if(mp.find(prefSum) != mp.end())  // Found the required prefix sum 
+		{
+			if(i - mp[prefSum] > 1) return true; // check if atleast 2 elements are there or not
+		}
+		else mp[prefSum] = i;
+	}
+
+	return false;
+}
