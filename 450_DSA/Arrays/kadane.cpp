2988be4113d9bd0420960edcca607a8256a22b13
Adithi <adithibhatindia@gmail.com>
2022-11-11 07:11:49 +0530

    Create kadane.cpp

---
+++ 450_DSA/Arrays/kadane.cpp
@@ -0,0 +1,20 @@
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        long long int ans = INT_MIN, sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            ans = max(sum,ans);
+            if(sum<0)
+                sum = 0;
+        }
+        return ans;
+        // Your code here
+        
+    }
+};
