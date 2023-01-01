8c1dd03ebb56b6d18345dc503c616a00a84a0546
Adithi <adithibhatindia@gmail.com>
2023-01-01 21:24:02 +0530

    Create 1.py

---
+++ Leetcode/2023/1.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/word-pattern/submissions/
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+
+        return (len(set(pattern)) ==
+                len(set(s)) ==
+                len(set(zip_longest(pattern,s))))
