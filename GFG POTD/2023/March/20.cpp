efeb68d6d1f8d95314662e36cf32f862f6930b3e
Adithi <adithibhatindia@gmail.com>
2023-03-20 09:06:11 +0530

    Create 20.cpp

---
+++ GFG POTD/2023/March/20.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    int shortestXYDist(vector<vector<char>> grid, int N, int M) {
+        // code here
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>>vis(N,vector<int>(M,0));
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(grid[i][j]=='X'){
+                    q.push({{i,j},0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+        int ans = INT_MAX;
+        int dr[] = {-1,1,0,0};
+        int dc[] = {0,0,-1,1};
+        
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            
+            int r = it.first.first;
+            int c = it.first.second;
+            int dist = it.second;
+            
+            if(grid[r][c]=='Y')
+                return dist;
+            
+            for(int i=0;i<4;i++){
+                int nr = r+dr[i];
+                int nc = c+dc[i];
+                
+                if(nr>=0 and nr<N and nc>=0 and nc<M and vis[nr][nc]==0){
+                    q.push({{nr,nc},dist+1});
+                    vis[nr][nc] = 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
